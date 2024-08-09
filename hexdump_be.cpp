3d6ee90686a334d6e28f11888e9e9eb8a10acbcd
Xen <twidlestick@gmail.com>
2024-08-09 12:43:06 +0530

    more formatting

--- hexdump_be.cpp
+++ hexdump_be.cpp
@@ -13,20 +13,23 @@ int main(int argc , char* argv[] ){
         return 1;
     }
 
+    int count = 0 ;
     unsigned short hex;
     while (file.read((char *)&hex, 2)) { 
+
+        if(count == 5){
+            count = 0;
+            std::cout << std::endl;
+        }
         hex = (hex << 8) | (hex >> 8) ;
         if(hex == 0){
-            std::cout << "00 \t";
+            std::cout << "0\t0\t";
         }
         else{
-        std::cout << std::hex << (hex >> 8);
-        std::cout << " \t";
-        std::cout << std::hex << (hex & 0x00ff);
-        std::cout << " \t";
+        std::cout << std::hex << (hex >> 8) << "\t";
+        std::cout << std::hex << (hex & 0x00ff) << "\t";
         }
-        
-      
+        count ++;
     }
     file.close();
     return 0;
