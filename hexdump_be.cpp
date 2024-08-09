c17b5241ab8434b50d269727d3920a6d52dacc5d
Xen <twidlestick@gmail.com>
2024-08-09 12:10:11 +0530

    printed

--- hexdump_be.cpp
+++ hexdump_be.cpp
@@ -12,15 +12,24 @@ int main(int argc , char* argv[] ){
         std::cerr << "Error opening file!" << std::endl;
         return 1;
     }
+    int count = 0 ;
     unsigned short hex;
     while (file.read((char *)&hex, 2)) { 
+        count +=2;
         hex = (hex << 8) | (hex >> 8) ;
+        if(hex == 0){
+            std::cout << "00 \t";
+        }
+        else{
         std::cout << std::hex << (hex >> 8);
-        std::cout << "\t";
+        std::cout << " \t";
         std::cout << std::hex << (hex & 0x00ff);
-        std::cout << "\t";
+        std::cout << " \t";
+        }
+        
       
     }
     file.close();
+    std::cout << "Total Bytes Read : " << count << std::endl;
     return 0;
 }
